64d4c6cf6152c751ef5b8d76a36ad4b1e202bb2f
Menosuno02 <mainusone1@gmail.com>
2024-02-12 19:19:39 +0100

    Update challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -3,7 +3,6 @@ const alfabeto = "abcdefghijklmnñopqrstuvwxyz";
 
 function quitarTildes(texto) {
   return texto
-    .toLowerCase()
     .replace("á", "a")
     .replace("é", "e")
     .replace("í", "i")
@@ -16,8 +15,14 @@ function cifrarCesar(texto, num) {
   let resultado = "";
   for (let i = 0; i < texto.length; i++) {
     let letra = texto[i];
-    if (alfabeto.indexOf(letra) != -1)
-      letra = alfabeto[(alfabeto.indexOf(letra) + num) % 27];
+    if (alfabeto.indexOf(letra.toLowerCase()) != -1) {
+      if (letra.toUpperCase() === letra)
+        letra =
+          alfabeto[
+            (alfabeto.indexOf(letra.toLowerCase()) + num) % 27
+          ].toUpperCase();
+      else letra = alfabeto[(alfabeto.indexOf(letra) + num) % 27];
+    }
     resultado += letra;
   }
   return resultado;
@@ -28,10 +33,11 @@ function descifrarCesar(texto, num) {
   let resultado = "";
   for (let i = 0; i < texto.length; i++) {
     let letra = texto[i];
-    if (alfabeto.indexOf(letra) != -1) {
-      let index = (alfabeto.indexOf(letra) - num) % 27;
+    if (alfabeto.indexOf(letra.toLowerCase()) != -1) {
+      let index = (alfabeto.indexOf(letra.toLowerCase()) - num) % 27;
       if (index < 0) index += 27;
-      letra = alfabeto[index];
+      if (letra.toUpperCase() === letra) letra = alfabeto[index].toUpperCase();
+      else letra = alfabeto[index];
     }
     resultado += letra;
   }
@@ -39,6 +45,6 @@ function descifrarCesar(texto, num) {
 }
 
 console.log(cifrarCesar("El perro de San Roque no tiene rabo", 13));
-console.log(descifrarCesar("qx cqeeb pq fnz ebdhq zb guqzq enñb", 13));
+console.log(descifrarCesar("Qx cqeeb pq Fnz Ebdhq zb guqzq enñb", 13));
 console.log(cifrarCesar("Ñoquis y tarta de queso, fresas y plátano!", 29)); // si desplazamiento > 27 sigue funcionando
-console.log(descifrarCesar("pqswku a vctvc fg swguq, htgucu a rncvcoq!", 29)); // si desplazamiento > 27 sigue funcionando
+console.log(descifrarCesar("Pqswku a vctvc fg swguq, htgucu a rncvcoq!", 29)); // si desplazamiento > 27 sigue funcionando
