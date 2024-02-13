8c8b106def8c5c663fcb8694f1616813d8460946
Menosuno02 <mainusone1@gmail.com>
2024-02-13 17:25:34 +0100

    Optimizar, juntar cipher y decipher, ASCII

--- challenge1/cifrado_cesar.js
+++ challenge1/cifrado_cesar.js
@@ -1,5 +1,5 @@
-// CIFRADO CÉSAR
-const alphabet = "abcdefghijklmnñopqrstuvwxyz";
+// CIFRADO CESAR
+// Alejandro López Labajo
 
 function vowelReplacements(text) {
   return text
@@ -10,42 +10,67 @@ function vowelReplacements(text) {
     .replace(/[úü]/g, "u");
 }
 
-function cipher(text, shift) {
+const alphabet = "abcdefghijklmnñopqrstuvwxyz";
+
+/**
+ * Cifrado Cesar (incluye ñ)
+ */
+function caesar(text, shift = 13, mode = "cipher") {
   text = vowelReplacements(text);
   let result = "";
   for (let i = 0; i < text.length; i++) {
     let letter = text[i];
-    if (alphabet.indexOf(letter.toLowerCase()) != -1) {
-      letter.toUpperCase() === letter
-        ? (letter =
-            alphabet[
-              (alphabet.indexOf(letter.toLowerCase()) + shift) % 27
-            ].toUpperCase())
-        : (letter = alphabet[(alphabet.indexOf(letter) + shift) % 27]);
+    let index = alphabet.indexOf(letter.toLowerCase());
+    if (index != -1) {
+      if (mode == "cipher") letter = alphabet[(index + shift) % 27];
+      else if (mode == "decipher") {
+        let pos = (index - shift) % 27;
+        if (pos < 0) pos += 27;
+        letter = alphabet[pos];
+      }
+      if (text[i].toUpperCase() === text[i]) letter = letter.toUpperCase();
     }
     result += letter;
   }
   return result;
 }
 
-function decipher(text, shift) {
+console.log("CESAR");
+console.log(caesar("El perro de San Roque no tiene rabo", 13, "cipher"));
+console.log(caesar("Qx cqeeb pq Fnz Ebdhq zb guqzq enñb", 13, "decipher"));
+console.log(caesar("Ñoquis, tarta de queso y plátano!", 29)); // si desplazamiento > 27 sigue funcionando
+console.log(caesar("Pqswku, vctvc fg swguq a rncvcoq!", 29, "decipher")); // si desplazamiento > 27 sigue funcionando
+
+/**
+ * Cifrado Cesar con ASCII (no incluye ñ)
+ */
+function caesarASCII(text, shift = 13, mode = "cipher") {
   text = vowelReplacements(text);
   let result = "";
   for (let i = 0; i < text.length; i++) {
-    let letter = text[i];
-    if (alphabet.indexOf(letter.toLowerCase()) != -1) {
-      let index = (alphabet.indexOf(letter.toLowerCase()) - shift) % 27;
-      if (index < 0) index += 27;
-      letter.toUpperCase() === letter
-        ? (letter = alphabet[index].toUpperCase())
-        : (letter = alphabet[index]);
+    let code = text.charCodeAt(i);
+    if (code >= 65 && code <= 90) {
+      // mayúsculas
+      if (mode == "cipher") code = ((code - 65 + shift) % 26) + 65;
+      else if (mode == "decipher") {
+        let pos = (code - 65 - shift) % 26;
+        if (pos < 0) pos += 26;
+        code = pos + 65;
+      }
+    } else if (code >= 97 && code <= 122) {
+      // minúsculas
+      if (mode == "cipher") code = ((code - 97 + shift) % 26) + 97;
+      else if (mode == "decipher") {
+        let pos = (code - 97 - shift) % 26;
+        if (pos < 0) pos += 26;
+        code = pos + 97;
+      }
     }
-    result += letter;
+    result += String.fromCharCode(code);
   }
   return result;
 }
 
-console.log(cipher("El perro de San Roque no tiene rabo", 13));
-console.log(decipher("Qx cqeeb pq Fnz Ebdhq zb guqzq enñb", 13));
-console.log(cipher("Ñoquis y tarta de queso, fresas y plátano!", 29)); // si desplazamiento > 27 sigue funcionando
-console.log(decipher("Pqswku a vctvc fg swguq, htgucu a rncvcoq!", 29)); // si desplazamiento > 27 sigue funcionando
+console.log("CESAR ASCII");
+console.log(caesarASCII("El perro de San Roque no tiene rabo", 13, "cipher"));
+console.log(caesarASCII("Ry creeb qr Fna Ebdhr ab gvrar enob", 13, "decipher"));
